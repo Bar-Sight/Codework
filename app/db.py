caf3564d19cc5ca6a36591a9cd5a2a3243908bb8
lesunshine_tuffkirkbaby <lucadaste5@gmail.com>
2026-04-14 21:25:26 -0700

    Delete app directory

--- app/db.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, create_engine, Session
-import os
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./barsight.db")
-engine = create_engine(DATABASE_URL, echo=False, connect_args={"check_same_thread": False})
-def init_db():
-    SQLModel.metadata.create_all(engine)
-def get_session():
-    return Session(engine)
